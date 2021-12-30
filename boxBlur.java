c953e4db8cf9a9523a05d76dda6c6b4a01f9eaac
SHihozs <88846962+SHihozs@users.noreply.github.com>
2021-12-30 20:31:03 +0700

    Add files via upload
    For
    
    image = [[1, 1, 1], 
                    [1, 7, 1], 
                    [1, 1, 1]]
    the output should be solution(image) = [[1]].
    
    To get the value of the middle pixel in the input 3 × 3 square: (1 + 1 + 1 + 1 + 7 + 1 + 1 + 1 + 1) = 15 / 9 = 1.66666 = 1. The border pixels are cropped from the final result.

---
+++ boxBlur.java
@@ -0,0 +1,42 @@
+int[][] solution(int[][] image) {
+    
+    //MAKE A ANSWER ARRAY (ROW)
+    int[][] array  = new int [image.length-2][];
+    
+    //MAKE A ANSWER ARRAY (COL)
+    for(int i=0; i<array.length; i++){
+        array[i] = new int [image[0].length-2];
+    }
+    
+    //CHECK 3x3
+    /*
+        1. use i for iden index of answer array (row) 
+        2. use j for iden index of answer array (col) 
+    */
+    
+    for(int r=0; r+3 <= image.length; r++){
+        for(int c=0; c+3 <= image[0].length;c++){
+            
+            int row = r;
+            int col = c;
+            int sum = 0;
+            
+            while(col < c+3 && row < r+3){
+                sum += image[row][col];
+                //CHANGE COL
+                col++;
+                
+                if(col >= c+3 && row < r+3){
+                    //CHANGE ROW
+                    row++;
+                    col = c;
+                }
+            }
+            
+            sum = sum/9;
+            array[r][c] = sum;
+        }
+    }
+    
+    return array;  
+}
