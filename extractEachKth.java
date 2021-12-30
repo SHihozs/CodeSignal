e6bb05f752c4e853cb856023f001742652c87731
SHihozs <88846962+SHihozs@users.noreply.github.com>
2021-12-30 20:39:04 +0700

    Update extractEachKth.java
    Given array of integers, remove each kth element from it.
    
    Example
    
    For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be solution(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].

--- extractEachKth.java
+++ extractEachKth.java
@@ -25,30 +25,3 @@ int[] solution(int[] inputArray, int k) {
     return result;
     
 }
-int[] solution(int[] inputArray, int k) {
-
-    /*
-        array    : [2, 4, 6, 8, 10]
-        position :  1  2  3  4   5
-        k: 2
-        result   :  [2, 6, 10]
-    
-    */
-    
-    Queue<Integer> queue = new LinkedList<>();
-    
-    for(int i=0; i<inputArray.length; i++){
-        if((i+1) % k != 0){
-            queue.add(inputArray[i]);
-        }
-    }
-    
-    int[] result = new int[queue.size()];
-    
-    for(int i=0; i<result.length; i++){
-        result[i] = queue.poll();
-    }
-    
-    return result;
-    
-}
