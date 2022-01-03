d44edcd254fadc46ecd4d3df949d03dae5ef1623
SHihozs <88846962+SHihozs@users.noreply.github.com>
2022-01-03 19:44:27 +0700

    06| Make Array Consecutive 2.java

---
+++ Intro/Edge of the Ocean/06| Make Array Consecutive 2.java
@@ -0,0 +1,11 @@
+int solution(int[] statues) {
+    Arrays.sort(statues); 
+    int result = 0;
+    
+    for(int i=0; i<statues.length-1; i++){
+        if(statues[i+1] - statues[i] > 1){
+            result += (statues[i+1] - statues[i])-1;
+        }
+    }
+    return result;
+}
