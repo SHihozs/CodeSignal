03d2c514108d48a80b7a5790cd0bf424ef8ed656
SHihozs <88846962+SHihozs@users.noreply.github.com>
2022-01-04 17:15:50 +0700

    27| variableName.java

---
+++ Intro/06| Rains of Reason/27| variableName.java
@@ -0,0 +1,15 @@
+boolean solution(String name) {
+    
+    //CHECK NUMERIC VALUE FOR FIRST INDEX
+    if(Character.isDigit(name.charAt(0))){
+        return false;
+    }
+    
+    for(int i = 0; i<name.length(); i++){
+        
+        if(!Character.isLetterOrDigit(name.charAt(i)) && name.charAt(i) != '_'){
+            return false;
+        }
+    }
+    return true;
+}
